cf54759a0e81436ee78db706a1d017755381b8b4
Edwin Ajong <edwinajong@gmail.com>
2022-05-06 21:35:18 +0100

    Setting class atributes

--- Class.py
+++ Class.py
@@ -1,2 +1,14 @@
 class 😸():
    pass
+
+class Edwin():
+   print(" I love myself")
+
+Edwin.name = "Ajong"
+
+class car():
+   def __init__(self, name):
+      self.name = name
+      print ("I love Edwin")
+
+hot = car("Sonia")
