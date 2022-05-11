4b47b7229be3b209df804b5364e4a6c72a3dd3b4
eddybruv <edwinajong@gmail.com>
2022-05-11 21:19:42 +0100

    check whether list is emply or not

---
+++ Basics/Exercise/01/08.py
@@ -0,0 +1,15 @@
+empty = []
+not_empty = [0, 2]
+
+
+def check(arr):
+    if len(arr) == 0:
+        print('Empty!!')
+        return
+    else:
+        print("Not empty")
+        return
+
+
+check(empty)
+check(not_empty)
